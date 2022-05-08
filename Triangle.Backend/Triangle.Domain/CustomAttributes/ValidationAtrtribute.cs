30fb72d737da40033d918ae2c6dbe6ccd97445ac
Oleg Hrobak <oleg.hrobak777@gmail.com>
2022-05-08 13:00:15 +0300

    Added class TraingleDto and added the validation to it

---
+++ Triangle.Backend/Triangle.Domain/CustomAttributes/ValidationAtrtribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Triangle.Domain.Dto;
+
+namespace Triangle.Domain.CustomAttributes
+{
+    public class ValidateTriangleAttribute : ValidationAttribute
+    {
+        public ValidateTriangleAttribute()
+        {
+            ErrorMessage = "Input sides are not a valid triangle.";
+        }
+        public override bool IsValid(object value)
+        {
+           
+            TriangleDto triangle = value as TriangleDto;
+
+            return (triangle.FirstSide + triangle.SecondSide < triangle.ThirdSide)
+                || (triangle.SecondSide + triangle.ThirdSide < triangle.FirstSide)
+                || (triangle.FirstSide + triangle.ThirdSide < triangle.SecondSide);
+        }
+    }
+}
---
+++ Triangle.Backend/Triangle.Domain/Dto/TriangleDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Triangle.Domain.CustomAttributes;
+
+namespace Triangle.Domain.Dto
+{
+    [ValidateTriangle]
+    public class TriangleDto
+    {
+        [Required]
+        public double FirstSide { get; set; }
+
+        [Required]
+        public double SecondSide { get; set; }
+
+        [Required]
+        public double ThirdSide { get; set; }
+    }
+}
