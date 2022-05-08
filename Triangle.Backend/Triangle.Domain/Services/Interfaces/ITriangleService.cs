ffa9f64f3996e3730ab8e912acbc0a0a5778bf13
Oleg Hrobak <oleg.hrobak777@gmail.com>
2022-05-08 12:20:34 +0300

    Added Triangle.Service

---
+++ Triangle.Backend/Triangle.Domain/Services/Interfaces/ITriangleService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle.Domain.Services.Interfaces
+{
+    public interface ITriangleService
+    {
+        public double GetPerimeter(double firstSide, double secondSide, double thirdSide);
+
+        public double GetArea(double firstSide, double secondSide, double thirdSide);
+    }
+}
